80f4c6779cf76eafda9c91a36956a5d9920c3dd9
sora0319 <sojeoung536985@gmail.com>
2023-05-11 21:41:17 +0900

    feat : solved boj 10171

---
+++ basic_codeII/No_10171.java
@@ -0,0 +1,15 @@
+package basic_codeII;
+import java.io.*;
+
+public class No_10171 {
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("\\    /\\\n");
+        bw.write(" )  ( ')\n");
+        bw.write("(  /  )\n");
+        bw.write(" \\(__)|\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
