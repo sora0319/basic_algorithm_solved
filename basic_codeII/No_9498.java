ce19af0576fd8e39b076f268da46dd7fa5e237a1
sora0319 <sojeoung536985@gmail.com>
2023-05-11 22:25:33 +0900

    feat : solved boj 9498

---
+++ basic_codeII/No_9498.java
@@ -0,0 +1,30 @@
+package basic_codeII;
+import java.io.*;
+
+public class No_9498 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        if(n >= 90) {
+            bw.write("A");
+        }
+        else if(n >= 80) {
+            bw.write("B");
+        }
+        else if(n >= 70) {
+            bw.write("C");
+        }
+        else if(n >= 60) {
+            bw.write("D");
+        }
+        else{
+            bw.write("F");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
