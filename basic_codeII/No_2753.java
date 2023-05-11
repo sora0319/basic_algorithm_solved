623fb7126f9cf727a2b7688a0605310c80fafc97
sora0319 <sojeoung536985@gmail.com>
2023-05-11 23:09:06 +0900

    feat : solved boj 2753

---
+++ basic_codeII/No_2753.java
@@ -0,0 +1,27 @@
+package basic_codeII;
+
+import java.io.*;
+
+public class No_2753 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int year = Integer.parseInt(br.readLine());
+
+        if (year % 4 == 0){
+            if (year % 400 == 0 || year % 100 != 0){
+                bw.write("1");
+            }
+            else{
+                bw.write("0");
+            }
+        }
+        else{
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
