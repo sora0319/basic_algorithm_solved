e0ffb2547b15987b09e4c73511acfd4f9601aa51
sora0319 <sojeoung536985@gmail.com>
2023-05-11 21:34:02 +0900

    feat : solved boj 2557

---
+++ basic_codeII/No_2557.java
@@ -0,0 +1,12 @@
+package basic_codeII;
+
+import java.io.*;
+
+public class No_2557 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+    }
+}
