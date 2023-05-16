99e1647a21d49440d736d6bdbc2be414fc767e52
sora0319 <sojeoung536985@gmail.com>
2023-05-16 21:27:16 +0900

    feat : solved boj 2440

---
+++ basic_codeII/No_2440.java
@@ -0,0 +1,24 @@
+package basic_codeII;
+
+import java.io.*;
+
+public class No_2440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = N; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
