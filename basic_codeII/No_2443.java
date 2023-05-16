aebcfeeec65e45b5a900c49fc761273938ae6412
sora0319 <sojeoung536985@gmail.com>
2023-05-16 22:37:26 +0900

    feat : solved boj 2443

---
+++ basic_codeII/No_2443.java
@@ -0,0 +1,25 @@
+package basic_codeII;
+
+import java.io.*;
+
+public class No_2443 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = N; i >= 1; i--){
+            for(int j = 1; j <= N-i; j++){
+                bw.write(" ");
+            }
+            for(int j = 1; j <= 2*i-1; j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
