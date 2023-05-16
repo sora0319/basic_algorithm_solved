5a3411112b6e36ce249f4bc29e569d1703a10b8a
sora0319 <sojeoung536985@gmail.com>
2023-05-16 19:55:38 +0900

    feat : solved boj 10093

---
+++ basic_codeII/No_10093.java
@@ -0,0 +1,37 @@
+package basic_codeII;
+
+import java.io.*;
+import java.util.*;
+
+public class No_10093 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] temp = br.readLine().split(" ");
+
+        long[] gap = new long[2];
+        for(int i = 0; i < 2; i++){
+            gap[i] = Long.parseLong(temp[i]);
+        }
+
+        Arrays.sort(gap);
+        if (gap[1] - gap[0] - 1 <= 0){
+            bw.write("0");
+            bw.write("\n");
+        }
+        else{
+            bw.write(String.valueOf(gap[1] - gap[0] - 1));
+            bw.write("\n");
+
+            for(long i = gap[0]+1; i < gap[1]; i++){
+                bw.write(String.valueOf(i));
+                bw.write(" ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
