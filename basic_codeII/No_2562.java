a0558b9d67f1730b647ba8f8b5eedac4765ffd94
sora0319 <sojeoung536985@gmail.com>
2023-05-16 22:38:24 +0900

    feat : solved boj 2562

---
+++ basic_codeII/No_2562.java
@@ -0,0 +1,29 @@
+package basic_codeII;
+
+import java.io.*;
+
+public class No_2562 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max = 0;
+        int order = 0;
+
+        for(int i = 1; i < 10; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(max < n){
+                max = n;
+                order = i;
+            }
+        }
+
+        bw.write(String.valueOf(max));
+        bw.write("\n");
+        bw.write(String.valueOf(order));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
