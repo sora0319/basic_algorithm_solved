9d09abad3e73bafa8933abffab0ba849b1f8cb56
sora0319 <sojeoung536985@gmail.com>
2023-08-01 11:10:09 +0900

    feat: add boj 1475

--- Arrays/code/No_1475.java
+++ Arrays/code/No_1475.java
@@ -1,7 +1,44 @@
 package Arrays.code;
 
+import java.io.*;
+
 public class No_1475 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] numbers = new int[10];
+
+        String temp = br.readLine();
+
+        int inputLength = temp.length();
+        int set = 0;
+        for (int i = 0; i < inputLength; i++){
+            int n = temp.charAt(i) - '0';
+
+            if(n == 6 || n == 9){
+                if(numbers[6] > numbers[9]){
+                    numbers[9]++;
+                }
+                else if(numbers[6] < numbers[9]){
+                    numbers[6]++;
+                }
+                else{
+                    numbers[n]++;
+                }
+            }
+            else{
+                numbers[n]++;
+            }
+
+            if (set < numbers[n]){
+                set = numbers[n];
+            }
+        }
+
+        bw.write(String.valueOf(set));
+        bw.flush();
+        br.close();
+        bw.close();
 
     }
 }
