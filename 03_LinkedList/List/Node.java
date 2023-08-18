dea208a892be9f8fb7c318bf446286f637b4cceb
sora0319 <sojeoung536985@gmail.com>
2023-08-18 21:39:48 +0900

    chore: node test

---
+++ 03_LinkedList/List/Node.java
@@ -0,0 +1,11 @@
+package List;
+
+public class Node {
+    private Node prev;
+    private Object data;
+    private Node next;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+}
