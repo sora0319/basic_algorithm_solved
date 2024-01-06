069a646a9e5513159e106561ed6d5153a864d5ea
sora0319 <sojeoung536985@gmail.com>
2024-01-06 23:59:05 +0900

    feat: add 1406 file

---
+++ 03_LinkedList/code/No_1406.java
@@ -0,0 +1,17 @@
+package code;
+
+import java.io.*;
+import java.util.*;
+public class No_1406 {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<String> words = new LinkedList<>();
+        ListIterator
+
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
