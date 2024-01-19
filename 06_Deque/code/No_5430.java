ba54443ab17286bee52cbf5b9c929fa639290abe
sora0319 <sojeoung536985@gmail.com>
2024-01-19 23:52:08 +0900

    feat: add boj 5430

---
+++ 06_Deque/code/No_5430.java
@@ -0,0 +1,58 @@
+package code;
+import java.io.*;
+import java.util.*;
+public class No_5430 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Integer>deque = new ArrayDeque<>();
+
+        int T = Integer.parseInt(br.readLine());
+        int n;
+
+        while(T-- > 0){
+            String order = br.readLine();
+            n = Integer.parseInt(br.readLine());
+
+            if(n >0) {
+                String[] inputs = br.readLine().substring(1, n * 2).split(",");
+                for (String s : inputs) {
+                    deque.offer(Integer.parseInt(s));
+                }
+            }
+
+            boolean forward = true;
+            boolean error = false;
+            for (int i = 0; i < order.length(); i++) {
+                if (order.charAt(i) == 'R') {
+                    forward = !forward;
+                } else if (order.charAt(i) == 'D') {
+                    if(deque.peek() == null){
+                        error = true;
+                        break;
+                    }
+                    if(forward){
+                        deque.pollFirst();
+                    }
+                    else{
+                        deque.pollLast();
+                    }
+                }
+            }
+
+            if(!error){
+                bw.write("[");
+                for (int d : deque) {
+                    bw.write(String.valueOf());
+                }
+            }
+
+
+            deque.clear();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
