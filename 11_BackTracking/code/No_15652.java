af9d947d578e94974ad1e401bc05a882793decf7
sora0319 <sojeoung536985@gmail.com>
2024-04-09 23:51:08 +0900

    feat: success boj 15652

--- 11_BackTracking/code/No_15652.java
+++ 11_BackTracking/code/No_15652.java
@@ -1,4 +1,36 @@
 package code;
 
+import java.io.*;
+import java.util.*;
 public class No_15652 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int N, M;
+    private static int[] sequence;
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sequence = new int[M];
+
+        createSubSeq(0,1);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    private static void createSubSeq(int order, int start) throws IOException{
+        if (order == M) {
+            for (int i : sequence) {
+                bw.write(String.valueOf(i));
+                bw.write(" ");
+            }
+            bw.newLine();
+            return;
+        }
+        for (int i = start; i <= N; i++) {
+            sequence[order] = i;
+            createSubSeq(order + 1, i);
+        }
+    }
 }
