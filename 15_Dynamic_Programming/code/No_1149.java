b649c962811fac47431972449ef2920a5862a35a
sora0319 <sojeoung536985@gmail.com>
2024-06-26 17:19:06 +0900

    feat: unfinish boj 1149

---
+++ 15_Dynamic_Programming/code/No_1149.java
@@ -0,0 +1,20 @@
+package code;
+
+import java.io.*;
+public class No_1149 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+
+/*
+1   2   3   4   5   6   7   8
+39
+
+ */
