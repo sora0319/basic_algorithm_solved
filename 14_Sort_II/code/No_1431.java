31a70052dec603e69749f5f62f406ed978b72c2e
sora0319 <sojeoung536985@gmail.com>
2024-09-05 09:08:44 +0900

    feat : finish boj 1431

--- 14_Sort_II/code/No_1431.java
+++ 14_Sort_II/code/No_1431.java
@@ -8,14 +8,17 @@ public class No_1431 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        String[] serial = new String[N];
+        Number[] numbers = new Number[N];
 
         for (int i = 0; i < N; i++) {
-            serial[i] = br.readLine();
+            numbers[i] = new Number(br.readLine());
         }
 
-        Arrays.sort(serial);
-        System.out.println(Arrays.toString(serial));
+        Arrays.sort(numbers);
+        for (Number n : numbers) {
+            bw.write(n.serial);
+            bw.newLine();
+        }
 
         br.close();
         bw.flush();
@@ -24,18 +27,39 @@ public class No_1431 {
     private static class Number implements Comparable<Number>{
         String serial;
         int length;
+        int sum;
 
-        Number(String serial, int length) {
+        Number(String serial) {
             this.serial = serial;
-            this.length = length;
+            this.length = serial.length();
+            this.sum = calculate(serial);
+        }
+
+        private int calculate(String serial) {
+            int sum = 0;
+            for (int i = 0; i < serial.length(); i++) {
+                int parseNumber = serial.charAt(i) - '0';
+                if (parseNumber >= 0 && parseNumber < 10) {
+                    sum += parseNumber;
+                }
+            }
+            return sum;
         }
 
         @Override
         public int compareTo(Number o) {
-            if(this.length < o.length){
-                return -1;
+            if(this.length < o.length) return -1;
+            if (this.length == o.length) {
+                if (this.sum < o.sum) return -1;
+                if (this.sum > o.sum) return 1;
+                for (int i = 0; i < this.length; i++) {
+                    if (this.serial.charAt(i) < o.serial.charAt(i)) return -1;
+                    if (this.serial.charAt(i) > o.serial.charAt(i)) return 1;
+                }
+                return 0;
             }
-
+            return 1;
         }
+
     }
 }
