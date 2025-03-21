9035cd8d1cd8463d14e885b3f44f65454b339c5e
sora0319 <sojeoung536985@gmail.com>
2025-03-21 20:20:59 +0900

    feat : finish boj 1463 (Dynamic Programming)

--- 15_Dynamic_Programming/code/No_1463.java
+++ 15_Dynamic_Programming/code/No_1463.java
@@ -6,11 +6,26 @@ public class No_1463 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int X = Integer.parseInt(br.readLine());
-        int[] dpArray = new int[X+1];
+        int[] counts = new int[X + 1];
+        counts[1] = 0;
 
-        dpArray[1] = 1;
+        for (int i = 2; i <= X; i++) {
+            int minCount = 1000001;
+            if (i % 3 == 0) {
+                minCount =  1 + counts[i/3];
+            }
+            if (i % 2 == 0) {
+                if(minCount > 1 + counts[i/2]){
+                    minCount = 1 + counts[i/2];
+                }
+            }
+            if(minCount > 1 + counts[i-1]){
+                minCount = 1 + counts[i-1];
+            }
+            counts[i] = minCount;
+        }
 
-
+        bw.write(Integer.toString(counts[X]));
 
         br.close();
         bw.flush();
